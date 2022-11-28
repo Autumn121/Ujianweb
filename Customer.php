eef6f0cc474ae6aa9bffa07a8aa0e4a057032ce9
Autumn121 <azw8907@gmail.com>
2022-11-28 15:03:23 +0700

    first commit

---
+++ Customer.php
@@ -0,0 +1,30 @@
+<?php
+
+class Customer {
+public $id, $name, $address, $country;
+}
+$customer1 = new Customer();
+$customer2 = new Customer();
+$customer3 = new Customer();
+$customer4 = new Customer();
+
+echo 'ID: ' . $customer1->id = 1 . '<br>';
+echo 'Nama: ' . $customer1->name = "Maria" . '<br>';
+echo 'Alamat: ' . $customer1->address = "Berlin" . '<br>';
+echo 'Negara: ' . $customer1->country = "German" . '<hr>';
+
+echo 'ID: ' . $customer2->id = 2 . '<br>';
+echo 'Nama: ' . $customer2->name = "Ana" . '<br>';
+echo 'Alamat: ' . $customer2->address = "Mexico" . '<br>';
+echo 'Negara: ' . $customer2->country = "Mexico" . '<hr>';
+
+echo 'ID: ' . $customer3->id = 3 . '<br>';
+echo 'Nama: ' . $customer3->name = "Antonio" . '<br>';
+echo 'Alamat: ' . $customer3->address = "Mexico" . '<br>';
+echo 'Negara: ' . $customer3->country= "Mexico" . '<hr>';
+
+echo 'ID: ' . $customer4->id = 4 . '<br>';
+echo 'Nama: ' . $customer4->name = "Thomas" . '<br>';
+echo 'Alamat: ' . $customer4->address = "London" . '<br>';
+echo 'Negara: ' . $customer4->country = "UK" . '<hr>';
+
